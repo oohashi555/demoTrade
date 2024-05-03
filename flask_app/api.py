d70c871d4fbbf65868c3fb591aadb24df3957f7a
oohashi <hanabi5479@gmail.com>
2024-05-03 23:02:09 +0900

    チャート表示

--- flask_app/api.py
+++ flask_app/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 import flask_app.models.user as user
+import yfinance as yf
 
 api = Blueprint('api', __name__)
 
@@ -27,4 +28,21 @@ def user_delete():
     form = request.get_json()
     user.delete(form)
     data = user.select()
+    return jsonify(data)
+
+@api.route('/getchart', methods=['POST'])
+def get_chart():
+    ticker = '9984.T'
+    
+    history = yf.download(ticker,period = '1y', interval = '1wk')
+    axes = history.axes[0]
+    open = history.Open.values
+    high = history.High.values
+    low = history.Low.values
+    close = history.Close.values
+    
+    data = []
+    for i in range(len(axes)):
+        data.append({'x':axes[i].timestamp() * 1000, 'o':open[i], 'h':high[i], 'l':low[i], 'c':close[i]})
+    
     return jsonify(data)
--- frontend/src/components/TopView.vue
+++ frontend/src/components/TopView.vue
@@ -1,3 +1,32 @@
+<script setup>
+import {ref} from 'vue'
+import axios from 'axios'
+import { onMounted } from 'vue'
+import {createCandleChart} from './candleStick'
+
+const ctx = ref(null)
+const errMsg = ref()
+const data = ref(null)
+
+function getChart(){
+	const path = 'http://localhost:5000/api/getchart'
+	axios.post(path, {})
+		.then(response => {
+			data.value = response.data
+			createCandleChart(ctx.value.getContext('2d'), data.value)
+			errMsg.value = ''
+		})
+		.catch(error => {
+			errMsg.value = 'データ取得に失敗しました'
+			console.log(error)
+		})
+}
+
+onMounted(() => {
+	getChart()	
+})
+</script>
 <template>
-	トップ
+	<p v-if="errMsg != ''" class="text-danger">{{errMsg}}</p>
+	<canvas id="chart" ref="ctx"></canvas>
 </template>
